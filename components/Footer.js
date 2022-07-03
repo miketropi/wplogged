3c8950249b44357a115aa4cddc424c6816afb239
Hiếu Huỳnh <hieuhuynh@MacBook-Air-cua-Hieu.local>
2022-07-03 21:58:24 +0700

    Update footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ const FooterInnerContainer = styled.div`
   color: white;
 `;
 
-export default () => {
+export default function Footer() {
   return <div id="SiteFooter" className="site-footer"> 
     <FooterInnerContainer>
       <Logo /> 
